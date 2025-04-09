9716a9f5a99914726563af45da9344ba0750cad6
kaandradeavila <141256253+kaandradeavila@users.noreply.github.com>
2025-04-08 23:41:39 -0400

    Solved all 3 challenges in this lesson

---
+++ Lesson 2/src/index.js
@@ -0,0 +1,11 @@
+// Challenge 1
+let buttonElementChallenge1 = document.querySelector("#challenge-1"); 
+buttonElementChallenge1.setAttribute("disabled", "disabled")
+
+//Challenge 2
+let buttonElementChallenge2 = document.querySelector("#challenge-2"); 
+buttonElementChallenge2.style.backgroundColor = "green"
+
+//Challenge 3
+let buttonElementChallenge3 = document.querySelector("#challenge-3");
+buttonElementChallenge3.classList.add("danger")
