bc569be5e6ed8943fe3f7e993690ec0e3bdcebc4
kaandradeavila <141256253+kaandradeavila@users.noreply.github.com>
2025-04-11 01:52:48 -0400

    Finished app functionality
    The functionality of the app is completed. I just need to make small adjustments to the CSS file

--- Homework Week 1/src/index.js
+++ Homework Week 1/src/index.js
@@ -1,12 +1,20 @@
 function showAnswer(response){
-    let generatedResponse = response.data.answer
-    let jokeResponse = document.querySelector("#joke-response")
-    jokeResponse.innerHTML = generatedResponse
+    let jokeContainer = document.querySelector("#joke-container")
+    jokeContainer.classList.add("joke-container")
+    
+    new Typewriter("#joke-response", {
+    strings: [response.data.answer],
+    autoStart: true,
+    cursor: "",
+    delay: 50,
+    deleteSpeed: 50,
+    });
+
 }
 
 function generateJoke(){
     let aiPrompt = "You are a funny dad who loves to joke around to bother his kids"
-    let aiContext = "Please, tell me a typical dad joke that would make your kids cringe"
+    let aiContext = "Please, tell me a typical dad joke that would make your kids cringe. Make your response simple as if you're telling the joke to someone."
     let apiKey = "424369doa037d0347bft3cfcc8cef956"
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${aiPrompt}&context=${aiContext}&key=${apiKey}`
     
