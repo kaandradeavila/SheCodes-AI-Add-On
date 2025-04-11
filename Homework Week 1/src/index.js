c3fb4b3ad607c8b351c0343f5e8cf3c67b49e135
kaandradeavila <141256253+kaandradeavila@users.noreply.github.com>
2025-04-11 01:28:54 -0400

    Generated AI response appears in Joke section
    The request and response successfully displays the joke in the section area. Missing typewriterJS functionality to be added.

--- Homework Week 1/src/index.js
+++ Homework Week 1/src/index.js
@@ -1,17 +1,16 @@
 function showAnswer(response){
-    console.log(response.data.answer)
+    let generatedResponse = response.data.answer
+    let jokeResponse = document.querySelector("#joke-response")
+    jokeResponse.innerHTML = generatedResponse
 }
 
 function generateJoke(){
-
+    let aiPrompt = "You are a funny dad who loves to joke around to bother his kids"
+    let aiContext = "Please, tell me a typical dad joke that would make your kids cringe"
+    let apiKey = "424369doa037d0347bft3cfcc8cef956"
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${aiPrompt}&context=${aiContext}&key=${apiKey}`
+    
+    axios.request(apiUrl).then(showAnswer) 
 }
 
-let jokeButton = document.querySelector("#joke-button")
-
-let aiPrompt = "You are a funny dad who loves to joke around to bother his kids"
-let aiContext = "Please, tell me a typical dad joke that would make your kids cringe"
-let apiKey = "424369doa037d0347bft3cfcc8cef956"
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${aiPrompt}&context=${aiContext}&key=${apiKey}`
-
-response = axios.request(apiUrl).then(showAnswer)
-
+document.getElementById("joke-button").addEventListener("click", generateJoke)
