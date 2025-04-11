70ac56db9055e4f9730366194d5ff91a8119323f
kaandradeavila <141256253+kaandradeavila@users.noreply.github.com>
2025-04-10 23:19:17 -0400

    Uploading solution for challenge 3

--- Lesson 3/src/index.js
+++ Lesson 3/src/index.js
@@ -1,4 +1,11 @@
 // Challenge 2
-let challenge2Element = document.querySelector("#challenge-2")
+let challenge2Element = document.querySelector("#challenge-2");
 
-challenge2Element.innerHTML = "Kyrenia Ailen Andrade Avila"
+challenge2Element.innerHTML = "Kyrenia Ailen Andrade Avila";
+
+// Challenge 3
+new Typewriter("#challenge-3", {
+  strings: ["Kyrenia Ailen Andrade Avila"],
+  autoStart: true,
+  cursor: "  ♥",
+});
